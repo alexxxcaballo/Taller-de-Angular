8e5e69e334dc176661326de81bf4ba9a82a8eea2
Bonilla34 <mcmariachi29@gmail.com>
2024-10-24 21:23:42 -0600

    Clase 5

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { CardComponent } from "../../components/card/card.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ui-home',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  verPerfil(){
-    alert("Ver Perfil");
-  }
-
-  conocerMas(){
-    alert("Conocer Mas");
-  }
-
-  goProducts(){
-    window.location.href = "/products";
-  }
+  email:string = '';
+  password:string = '';
+   login(user:string, password:string){
+     console.log(`User ${user}, password ${password}`);
+   }
 }
