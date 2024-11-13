42a022dc3422a6e507f70e3fac939afa6be72d6f
Bonilla34 <mcmariachi29@gmail.com>
2024-11-12 19:39:19 -0600

    Clase 6

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { CardComponent } from "../../components/card/card.component";
 import { FormsModule } from '@angular/forms';
+import { ReqresService } from '../../services/reqres.service';
+import { error } from 'console';
 
 @Component({
   selector: 'ui-home',
@@ -13,7 +15,20 @@ import { FormsModule } from '@angular/forms';
 export class HomeComponent {
   email:string = '';
   password:string = '';
+
+  constructor(private apiReqres:ReqresService){}
+
    login(user:string, password:string){
      console.log(`User ${user}, password ${password}`);
+     this.apiReqres.Login(user, password).subscribe(
+      res => {
+        alert("Login successfull "+ res.token)
+        console.log("Login succesfull");
+        console.log(res.token);
+      },
+      err => {
+        alert(err.error.error);
+      }
+    )
    }
 }
---
+++ src/app/services/reqres.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReqresService {
+  private body(data:any){
+    return JSON.stringify(data);
+  }
+
+  private header(){
+    return {
+      headers: {
+        'Content-Type':'application/json'
+      }
+    }
+  }
+
+  private url(endpoint:string){
+    return `https://reqres.in/api/${endpoint}`;
+  }
+  constructor(private http:HttpClient) { }
+
+  public Login(user:string, password:string):Observable<any>{
+    return this.http.post<any>(
+      this.url('login'),
+      this.body({
+        email: user,
+        password
+      }),
+      this.header()
+    );
+    }
+}
